a011fca1bcb9cd929337e9471279b36ae1b2d8e0
xitecs113-stack <xitecs113@gmail.com>
2025-10-17 07:20:56 -0400

    Update and rename omni_a07.mk to twrp_a07.mk

--- twrp_a07.mk
+++ twrp_a07.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a07 device
 $(call inherit-product, device/samsung/a07/device.mk)
 
 PRODUCT_DEVICE := a07
-PRODUCT_NAME := omni_a07
+PRODUCT_NAME := twrp_a07
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A075M
 PRODUCT_MANUFACTURER := samsung
