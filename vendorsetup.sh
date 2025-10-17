4acf9d6aae38746b63e42d593dfeabef64b00907
xitecs113-stack <xitecs113@gmail.com>
2025-10-17 07:21:15 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a07-user
-add_lunch_combo omni_a07-userdebug
-add_lunch_combo omni_a07-eng
+add_lunch_combo twrp_a07-user
+add_lunch_combo twrp_a07-userdebug
+add_lunch_combo twrp_a07-eng
